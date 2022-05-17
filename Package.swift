dd71663ec53398ee7335eb541b6b2417b2b42c5e
Valsamis Elmaliotis <viel1985@icloud.com>
2022-05-17 17:55:02 +0300

    v 0.0.1

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SportBuff",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "SportBuff",
+            targets: ["SportBuff"]),
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(
+            name: "SportBuff",
+            url: "https://buffup-public.s3.eu-west-2.amazonaws.com/ios-sdk/sportbuff-ios-sdk-0.2.1.28-beta.zip",
+            checksum: "c76b8a71f7466811012bf2ab3a6e835e536b87adb877838e54142a4f8f6d913d"
+        )
+    ]
+)
